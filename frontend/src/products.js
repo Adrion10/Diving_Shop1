129191fc89a1fa4460f456df2d2d9f81ffcdd68f
Adrion10 <lacehila@gmail.com>
2022-06-17 23:59:53 +0200

    add products

---
+++ frontend/src/products.js
@@ -0,0 +1,89 @@
+const products = [
+  {
+    _id: "1",
+    name: "Iphone xs",
+    image:
+      "https://images.pexels.com/photos/7742555/pexels-photo-7742555.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 1000,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: "2",
+    name: "iPhone",
+    image:
+      "https://images.pexels.com/photos/4071887/pexels-photo-4071887.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 1200,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: "3",
+    name: "iPhone 12 Pro",
+    image:
+      "https://images.unsplash.com/photo-1608547492806-7e6c70ffdea4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 1300,
+    countInStock: 10,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: "4",
+    name: "Galaxy s21 plus",
+    image:
+      "https://images.unsplash.com/photo-1610945415295-d9bbf067e59c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 900,
+    countInStock: 10,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: "5",
+    name: "Samsung node 11",
+    image:
+      "https://images.pexels.com/photos/50614/pexels-photo-50614.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 1500,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 15,
+  },
+
+  {
+    _id: "6",
+    name: "Samsung s20 pro",
+    image:
+      "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+
+    description: "This is the best phone ever",
+    brand: "apple",
+    category: "mobile",
+    price: 1100,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 23,
+  },
+];
+
+export default products;
