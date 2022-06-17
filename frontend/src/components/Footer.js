3f1f42dd0015d41b4584a1006d725d5be77a0e2b
Adrion10 <lacehila@gmail.com>
2022-06-17 22:28:48 +0200

    container footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className="text-center py-3">
+            <p>Copyright &copy;DivingShop 2022</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
