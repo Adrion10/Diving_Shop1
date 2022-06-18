8da718d5f9ed4698b3e81a57901179bb62f386b4
Adrion10 <lacehila@gmail.com>
2022-06-18 22:32:58 +0200

    homescreen

--- frontend/src/components/screen/HomeScreen.js
+++ frontend/src/components/screen/HomeScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import products from "../products";
+import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
@@ -10,13 +11,7 @@ const HomeScreen = () => {
         {products.map((product) => {
           return (
             <Col sm={12} md={6} lg={4} xl={3} key={product.id}>
-              <h3>{product.name}</h3>
-              <img src={product.image} alt={product.name} />
-              <div className="card-body">
-                <h5 className="card-title">{product.name}</h5>
-                <p className="card-text">{product.description}</p>
-                <p className="card-text">${product.price}</p>
-              </div>
+              <Product product={product} />
             </Col>
           );
         })}
