8f2da836236e574cc8bcbb587838f793e31e31fa
Adrion10 <lacehila@gmail.com>
2022-06-18 23:05:59 +0200

    work in product price name, rating

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -14,6 +14,12 @@ const Product = ({ product }) => {
             <strong> {product.name} </strong>
           </Card.Title>
         </a>
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews}reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">€{product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
