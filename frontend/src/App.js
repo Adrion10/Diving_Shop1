e5d39b3081109c32964ae38def1b5a876c1ab6e6
Adrion10 <lacehila@gmail.com>
2022-06-19 23:05:12 +0200

    route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./components/screen/HomeScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
           <h1>Diving Shop</h1>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+          </Routes>
           <HomeScreen />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
