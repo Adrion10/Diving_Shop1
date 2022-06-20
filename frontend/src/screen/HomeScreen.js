4ae85a7cb0d43cd597b5e7c9f1e0eb7c9d42c3ea
Adrion10 <lacehila@gmail.com>
2022-06-20 23:22:40 +0200

    fixed

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import products from "../components/products";
+import products from "../products";
 import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
 
