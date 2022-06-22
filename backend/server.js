b2841360b1ceff509997aed690077e3087e2b3d1
Adrion10 <lacehila@gmail.com>
2022-06-22 23:47:22 +0200

    get product

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const products = require("./data/products");
 
 app.get("/", (req, res) => {
   res.send("API is Running");
+});
+app.get("/api/products", (req, res) => {
+  res.json(products);
 });
 
 app.listen(4000, console.log("Server running on port 4000 "));
