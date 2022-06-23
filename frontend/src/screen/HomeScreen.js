ea7173a5c58a3afd0fc4af9ed5b63dc0a9bfac20
Adrion10 <lacehila@gmail.com>
2022-06-23 23:07:59 +0200

    Homescreen

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,14 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import products from "../products";
+
 import Product from "../components/Product";
 import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const [products, setProducts] = useState([]);
   useEffect(() => {
-    const
-  })
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h2>PRODUCTS</h2>
