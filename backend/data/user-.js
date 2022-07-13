44947e4724d2d484d3fa5abbb04fca9302b6ae51
Adrion10 <lacehila@gmail.com>
2022-07-13 23:33:16 +0200

    fix

--- backend/data/user-.js
+++ backend/data/user-.js
@@ -3,5 +3,16 @@ const users = [
     name: "Admin User",
     email: "admin@example.com",
     password: "password",
+    isAdmin: true,
+  },
+  {
+    name: "Noel",
+    email: "noel@example.com",
+    password: "password",
+  },
+  {
+    name: "lace",
+    email: "lace@example.com",
+    password: "password",
   },
 ];
