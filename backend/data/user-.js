66ec65c9593977ad82eba09217cb3ddaed63dbbb
Adrion10 <lacehila@gmail.com>
2022-07-13 23:42:29 +0200

    fix

--- backend/data/user-.js
+++ backend/data/user-.js
@@ -1,18 +1,21 @@
+const bcrypt = require("bcryptjs");
 const users = [
   {
     name: "Admin User",
     email: "admin@example.com",
-    password: "password",
+    password: bcrypt.hashSync('123456',10),
     isAdmin: true,
   },
   {
     name: "Noel",
     email: "noel@example.com",
-    password: "password",
+    password: bcrypt.hashSync('123456',10),
+    password: ,
   },
   {
     name: "lace",
     email: "lace@example.com",
-    password: "password",
+    password: bcrypt.hashSync('123456',10),
+
   },
 ];
